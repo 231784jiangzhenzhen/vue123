21e8d8defae3c8e741b0c643556161d571dbae00
jiangzhenzhen <231784jiang@sina.com>
2017-01-21 17:51:39 +0800

    '第一次上传代码'

---
+++ my-fist-ve-project/src/AppCase1.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+   <h1 v-html="title"></h1>
+   <ul>
+   	<li v-for="item in items" v-bind:class="{finished:item.isFinished}">
+   		{{item.label}}
+   	</li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+ data:function(){
+ 	return {
+ 		title:'this is a todo list',
+ 		items:[
+ 			{
+ 				label:'coding',
+ 				isFinished:false
+ 			},
+ 			{
+ 				label:'walking',
+ 				isFinished:true
+ 			}
+ 		],
+ 		liClass:'thisisLiClass'
+ 	}
+ }
+}
+</script>
+
+<style>
+.finished{
+	text-decoration: underline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-fist-ve-project/src/AppCase2.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+   <h1 v-html="title"></h1>
+  
+   <ul>
+   	<li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">
+   		{{item.label}}
+   	</li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+ data:function(){
+ 	return {
+ 		title:'this is a todo list',
+ 		items:[
+ 			{
+ 				label:'coding',
+ 				isFinished:false
+ 			},
+ 			{
+ 				label:'walking',
+ 				isFinished:true
+ 			}
+ 		]
+ 		}
+ },
+ 	methods:{
+ 			toggleFinish:function(item){
+ 				item.isFinished =!item.isFinished
+ 			}
+ }
+}
+</script>
+
+<style>
+.finished{
+	text-decoration: underline;
+	color: red;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
